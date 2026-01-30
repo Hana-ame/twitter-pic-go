c5bf25ad2c23da7c7f341906993e69d043d00ab1
lumin <luminovoez@gmail.com>
2026-01-30 19:44:13 +0800

    自动构建

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 cd server
 
 go build .
-~/script/scp.sh server root@cloudcone.moonchan.xyz:~/twitter/temp
-~/script/scp.sh ../.env root@cloudcone.moonchan.xyz:~/twitter
-~/script/scp.sh ../get_meta_data.py root@cloudcone.moonchan.xyz:~/twitter
-~/script/scp.sh ../caller.py root@cloudcone.moonchan.xyz:~/twitter
-~/script/scp.sh ../bans.txt root@cloudcone.moonchan.xyz:~/twitter
+~/script/scp.sh server root@vps.moonchan.xyz:~/twitter/temp
+~/script/scp.sh ../.env root@vps.moonchan.xyz:~/twitter
+~/script/scp.sh ../get_meta_data.py root@vps.moonchan.xyz:~/twitter
+~/script/scp.sh ../caller.py root@vps.moonchan.xyz:~/twitter
+~/script/scp.sh ../bans.txt root@vps.moonchan.xyz:~/twitter
 
 cd -
 
